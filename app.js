996b70a3c9095f03568670338dde6c42c3ada993
David Ang <cdang1024@gmail.com>
2020-02-11 23:00:27 -0500

    added some express and path usage

--- app.js
+++ app.js
@@ -1,18 +1,10 @@
-
-
+const express = require('express');
 const http = require('http');
-
-const hostname = '127.0.0.1';
+const path = require('path');
 const port = process.env.PORT||5000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World this is the new and improved app');
-
-});
-
-server.listen(process.env.PORT || 5000, function() {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
-
+express()
+    .use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .listen(port, () => console.log(`Listening on ${ port }`))
