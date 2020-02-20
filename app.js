027b0a042f019a8a13efae92cca961e7793cab46
David Ang <cdang1024@gmail.com>
2020-02-19 23:29:53 -0500

    added ALL THE FUNCTIONALITY

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
+
 const express = require('express');
-const http = require('http');
+var app = express();
 const path = require('path');
 const port = process.env.PORT||5000;
+const http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-express()
-    .use(express.static('public'))
-    .set('views', path.join(__dirname, 'views'))
-    .set('view engine', 'ejs')
-    .get('/', function (req, res){
-        res.render('index')
+
+app.use(express.static('public'))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', function (req, res){ res.render('index') })
+http.listen(port, () => console.log(`Listening on ${ port }`))
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+      console.log('user disconnected');
+    });
+    socket.on('play', function(msg){
+        console.log(msg);
+        socket.broadcast.emit('returnplay', 'the server is broadcasting "play"');
     })
-    .listen(port, () => console.log(`Listening on ${ port }`))
+    socket.on('pause', function(msg){
+        console.log(msg);
+        socket.broadcast.emit('returnpause', 'the server is broadcasting "pause"');
+    })
+});
